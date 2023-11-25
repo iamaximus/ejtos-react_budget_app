cc5f1b815e4d176702156f979fceb3b686b88119
max <max@max.max.virtualbox.org>
2023-11-25 16:43:36 -0500

    validations

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,14 +4,26 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const [error, setError] = useState(null);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const value = event.target.value;
+
+        if (value <= 20000) {
+            setNewBudget(value);
+            setError(null);
+        } else {
+            setError("Budget cannot exceed £20,000");
+        }
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+            {error && <div style={{ color: 'red' }}>{error}</div>}
+        </div>
     );
 };
+
 export default Budget;
